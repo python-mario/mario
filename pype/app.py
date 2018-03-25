c88c1f115db898f78cebe9ace9a78da29ae40cf8
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-03-25 14:37:08 -0700

    help text

--- pype/app.py
+++ pype/app.py
@@ -37,13 +37,26 @@ def main(command, in_stream, imports, placeholder):
 
 
 @click.command()
-@click.option('--import', '-i', 'imports', type=str, multiple=True)
-@click.option('--placeholder', '-p', type=str, default='?')
-@click.argument('command')
+@click.option('--import', '-i', 'imports', type=str, multiple=True, help='Modules to import')
+@click.option('--placeholder', '-p', type=str, default='?', help='String to replace with data. Defaults to ?')
+@click.argument('command', type=str)
 @click.argument('in_stream', default=click.get_text_stream('stdin'), required=False)
 def cli(imports, command, in_stream, placeholder):
-    """Pipe data through python functions."""
+    """
+Pipe data through python functions.
+
+\b
+printf 'a.b.c\\nd.e.f\\n' |
+pype -i collections -i json 'str.replace(?, ".", "!") || str.upper || collections.Counter || dict || json.dumps '
+
+\b
+{"A": 1, "!": 2, "B": 1, "C": 1, "\\n": 1}
+{"D": 1, "!": 2, "E": 1, "F": 1, "\\n": 1}
+
+
+
+    """
     gen = main(command, in_stream, imports, placeholder)
     for line in gen:
-        click.echo(line, nl=False)
+        click.echo(line, nl=True)
     click.echo()
