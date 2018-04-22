e54e22d3a14324a2c28bdbefcc16c93d8a75a169
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-22 00:20:52 -0700

    Fix version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/me/myproject'
 EMAIL = 'me@example.com'
 AUTHOR = 'Me'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 0.0.1
+VERSION = '0.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
