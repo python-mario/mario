fd6ef8fec482f16232c4a81d014f3bacc9526e3f
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-22 09:23:00 -0700

    Add hypothesis profile

--- pype/tests/test_pype.py
+++ pype/tests/test_pype.py
@@ -9,11 +9,22 @@ import pytest
 from click.testing import CliRunner
 from hypothesis import given
 import hypothesis.strategies as st
+import os
+from hypothesis import settings, Verbosity
+
+
 import toolz
 
 import pype
 import pype.app
 from pype.app import _PYPE_VALUE
+
+settings.register_profile("ci", max_examples=1000)
+settings.register_profile("dev", max_examples=10)
+settings.register_profile(
+    "debug", max_examples=10, verbosity=Verbosity.verbose
+)
+settings.load_profile(os.getenv('HYPOTHESIS_PROFILE', 'default'))
 
 
 @pytest.fixture(name='runner')
