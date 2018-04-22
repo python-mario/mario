b9278254626003b12e07a67d32edbf423b1cef1e
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-22 00:12:19 -0700

    License

--- setup.py
+++ setup.py
@@ -103,11 +103,11 @@ setup(
     },
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license='Apache License 2.0',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
