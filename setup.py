3f2039228e7d36c6f43bfddb5267e8e1df56056a
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-22 00:17:58 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = 0.0.1
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'click',
+    'click', 'toolz',
 ]
 
 # The rest you shouldn't have to touch too much :)
