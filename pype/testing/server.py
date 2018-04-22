479be99b6a5d4c8e7e79362d4fb595e12ae83762
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-22 14:47:47 -0700

    Set up testing server

--- pype/testing/server.py
+++ pype/testing/server.py
@@ -1,15 +1,25 @@
 import asyncio
 
 from aiohttp import web
+import attr
 
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    await asyncio.sleep(10)
-    return web.Response(text=text)
+@attr.s
+class Handler:
+
+    counter = attr.ib(default=0)
+
+    async def handle(self, request):
+        name = request.match_info.get('name', "Anonymous")
+        text = f"Hello, {name}. You are client number {self.counter} for this server."
+        await asyncio.sleep(2)
+        self.counter += 1
+        return web.Response(text=text)
+
 
 app = web.Application()
+handler = Handler()
+handle = handler.handle
 app.add_routes([web.get('/', handle),
                 web.get('/{name}', handle)])
 
