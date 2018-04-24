3a8f67aea4e343f953d1120ce0a94ec0ee9c2626
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-24 01:46:21 -0700

    Show both messages

--- pype/app.py
+++ pype/app.py
@@ -68,8 +68,7 @@ class _StringScanner:
     def _maybe_update(self):
         if self._current_tokens and _is_name_token(self._current_tokens[-1]):
             if _is_name_token(self._current_tokens[0]):
-                self._identifier_strings.add(
-                    _tokens_to_string(self._current_tokens))
+                self._identifier_strings.add(_tokens_to_string(self._current_tokens))
             self._current_tokens = []
 
     def scan(self):
@@ -152,8 +151,7 @@ def _get_modules(commands, named_imports, autoimport):
     named_modules = _get_named_modules(named_imports)
     if not autoimport:
         return named_modules
-    autoimports = toolz.merge(_get_autoimports(command)
-                              for command in commands)
+    autoimports = toolz.merge(_get_autoimports(command) for command in commands)
     # named modules have priority
     modules = {**autoimports, **named_modules}
     return modules
@@ -224,14 +222,31 @@ def _check_parsing(command, placeholder):
             continue
         if placeholder not in tok.string:
             continue
+        if re.fullmatch(r'f.*\{.*%s.*\}.*' % placeholder, tok.string):
+            continue
 
         other = {'$': '?', '?': '$'}[placeholder]
-        raise PypeParseWarning(rf'''
+        raise PypeParseWarning(r'''
 
-        Use another placeholder value when quoting the placeholder:
+        If data should appear in quotes, use f-string format :
 
-        pype --placeholder={other} {command} ...
-            ''')
+
+           printf 'eggs' | pype 'f"Ham and {{?}} and spam!".upper()'
+
+           # HAM AND EGGS AND SPAM!
+
+
+           printf 'World' | pype $'f\'I say, "Hello, {{?}}!"\''
+
+           # I say, "Hello, World!"
+
+
+        If {placeholder} should appear in quotation marks, use another placeholder:
+
+
+        printf 'Is this a question' | pype --placeholder=$ '$ + "?"'
+
+            '''.format(placeholder=placeholder, other=other))
 
 
 def main(  # pylint: disable=too-many-arguments
@@ -249,15 +264,12 @@ def main(  # pylint: disable=too-many-arguments
     _check_parsing(mapper, placeholder)
 
     if slurp:
-        result = _apply_total(mapper, in_stream, imports,
-                              placeholder, autoimport)
+        result = _apply_total(mapper, in_stream, imports, placeholder, autoimport)
     else:
-        result = _apply_map(mapper, in_stream, imports,
-                            placeholder, autoimport)
+        result = _apply_map(mapper, in_stream, imports, placeholder, autoimport)
 
     if reducer is not None:
-        result = _apply_reduce(reducer, result, imports,
-                               placeholder, autoimport)
+        result = _apply_reduce(reducer, result, imports, placeholder, autoimport)
     if postmap is not None:
         result = _apply_map(postmap, result, imports, placeholder, autoimport)
 
