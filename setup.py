a88d7ccb99c9a5ebb2fba9e58507049d885f788f
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2018-04-29 01:25:56 -0700

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/me/myproject'
 EMAIL = 'me@example.com'
 AUTHOR = 'Me'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
