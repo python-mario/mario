5122388bbe6f0bdf4642209237f81c4ccfd7ceff
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-04-08 12:55:54 -0700

    Add json logs to testing server

--- tests/server.py
+++ tests/server.py
@@ -1,25 +1,47 @@
+import json
 import asyncio
+import datetime
+import contextvars
+import itertools
 
 from aiohttp import web
 import attr
 
 
+START_TIME = None
+counter = itertools.count()
+ID = contextvars.ContextVar("id")
+
+
 @attr.s
 class Handler:
-
-    counter = attr.ib(default=0)
-
     async def handle(self, request):
-        name = request.match_info.get("name", "Anonymous")
-        text = f"Hello, {name}. You are client number {self.counter} for this server."
-        await asyncio.sleep(2)
-        self.counter += 1
-        return web.Response(text=text)
+        global START_TIME
+        if START_TIME is None:
+            START_TIME = datetime.datetime.utcnow()
+
+        ID.set(next(counter))
+        delay = request.rel_url.query["delay"]
+
+        elapsed = (datetime.datetime.utcnow() - START_TIME).seconds
+        print(
+            json.dumps(
+                dict(message="receive", id=ID.get(), elapsed=elapsed, delay=delay)
+            )
+        )
+        await asyncio.sleep(int(delay))
+        elapsed = (datetime.datetime.utcnow() - START_TIME).seconds
+        response = json.dumps(
+            dict(message="respond", id=ID.get(), elapsed=elapsed, delay=delay)
+        )
+        print(response)
+
+        return web.Response(text=response)
 
 
 app = web.Application()
 handler = Handler()
 handle = handler.handle
-app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+app.add_routes([web.get("/", handle), web.get("/delay/{delay}", handle)])
 
 web.run_app(app)
