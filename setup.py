198951bce781565c6a01093b3b93e372161e555b
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-04-12 14:28:29 -0700

    Switch from poetry to setuptools

---
+++ setup.py
@@ -0,0 +1,38 @@
+import re
+
+import setuptools
+
+with open('requirements.in') as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+
+setuptools.setup(
+    name="pype",
+    version="0.0.82",
+    description="An example package. Generated with cookiecutter-pylibrary.",
+    long_description=open('README.rst').read(),
+    author="pype contributors",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Utilities",
+    ],
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    python_requires=">=3.7",
+    install_requires=INSTALL_REQUIRES,
+
+    entry_points={"console_scripts": ["pype = pype.cli:cli"]},
+)
