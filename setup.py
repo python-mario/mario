2f804f6370787fc9e64947881a88529164647807
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-04-22 20:41:31 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.in") as f:
 
 setuptools.setup(
     name="mario",
-    version="0.0.93",
+    version="0.0.94",
     description="Shell pipes for Python.",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
