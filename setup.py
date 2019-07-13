9d07a857fa120f741f902bd15b354ca469026326
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-13 23:49:03 +0000

    Bump version: 0.0.113 → 0.0.114

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(PROJECT_ROOT / "dev-requirements.in") as f:
 
 setuptools.setup(
     name="mario",
-    version="0.0.113",
+    version="0.0.114",
     description="Shell pipes for Python.",
     long_description=open(PROJECT_ROOT / "README.rst").read(),
     long_description_content_type="text/x-rst",
