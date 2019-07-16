32fc2f7ce5f5127e803deeabbe9458bbf7e71b17
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-16 18:46:15 +0000

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = "mario"
 year = "2019"
 author = "mario contributors"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.0.115"
+version = release = "0.0.116"
 
 pygments_style = "trac"
 templates_path = ["."]
