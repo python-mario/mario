a7202931de9fe4d81b58832504167d7714b6ba73
akb <uBX6aXPqDSRrt92kha28@cordaz.com>
2019-07-19 02:54:27 +0000

    Update broken cl.py

--- cl.py
+++ cl.py
@@ -1,55 +1,102 @@
 #!/usr/bin/env python3
 
+import attr
 import click
 
 
-@click.group(chain=True)
+class SectionedHelpCommand(click.Command):
+    """Sections commands into help groups"""
+
+    def command(self, *args, **kwargs):
+        help_group = kwargs.pop("help_group")
+        decorator = super().command(*args, **kwargs)
+
+        def new_decorator(f):
+            cmd = decorator(f)
+            cmd.help_group = help_group
+            self.grouped_commands.setdefault(help_group, []).append(cmd)
+            return cmd
+
+        return new_decorator
+
+    def format_options(self, ctx, formatter):
+        grouped_commands = {}
+        for param in self.params:
+            if hasattr(param, "for_command"):
+                grouped_commands.setdefault(param.for_command, []).append(param.name)
+
+        for group, cmds in grouped_commands.items():
+            rows = []
+            [choice_argument] = [
+                param
+                for param in self.params
+                if isinstance(param, click.Argument)
+                and isinstance(param.type, click.Choice)
+            ]
+            subcommand_name = ctx.params[choice_argument.name]
+            for param in self.params:
+
+                if param.name in [choice_argument.name, "help"]:
+                    continue
+                if not param.name in grouped_commands[subcommand_name]:
+                    continue
+                rows.append(param.get_help_record(ctx))
+
+            if rows:
+                with formatter.section(group):
+                    formatter.write_dl(rows)
+
+
+@click.group(chain=True, add_help_option=False)
 def cli():
     pass
 
 
 @cli.command()
-def greet():
+@click.argument("arg", type=click.Choice([int, float]))
+def greet(arg):
     print("hello")
 
 
-@cli.group()
-def read():
-    pass
+def read_callback(ctx, **kwargs):
+    if kwargs["help"]:
+
+        print(read.get_help(ctx))
+        return
+
+    print("read", kwargs)
 
 
-@cli.group()
-def write():
-    pass
+class OptionForCommand(click.Option):
+    def __init__(self, *args, for_command, **kwargs):
+        self.for_command = for_command
+        super().__init__(*args, **kwargs)
 
 
-@read.command()
-@click.option("--header/--no-header")
-def csv(header):
-    print("reading csv")
+read = SectionedHelpCommand(
+    "read",
+    callback=click.pass_context(read_callback),
+    params=[
+        click.Argument(["format"], type=click.Choice(["json", "csv"])),
+        click.Option(["--help"], is_flag=True, type=bool),
+        OptionForCommand(
+            ["--header/--no-header"], help="Is there a header?", for_command="csv"
+        ),
+        OptionForCommand(
+            ["--some-json-option"], help="Something about json", for_command="json"
+        ),
+    ],
+    add_help_option=False,
+)
+
+cli.add_command(read)
 
 
-@write.command()
-def json():
-    print("writing json")
+@cli.command(add_help_option=False)
+@click.argument("format")
+def write(format):
+    print("write", format)
 
 
 if __name__ == "__main__":
     cli()
-
-
-# With chain=False,
-# cl.py read csv write json
-# Usage: cl.py read csv [OPTIONS]
-# Try "cl.py read csv --help" for help.
-
-# Error: Got unexpected extra arguments (write json)
-
-
-# With chain=True,
-# cl.py read csv write json
-# Traceback (most recent call last):
-#   File "cl.py", line 16, in <module>
-#     @cli.group()
-#  ...
-# RuntimeError: It is not possible to add multi commands as children to another multi command that is in chain mode.  Command "cli" is set to chain and "read" was added as subcommand but it in itself is a multi command.  ("read" is a Group within a chained Group named "cli")
